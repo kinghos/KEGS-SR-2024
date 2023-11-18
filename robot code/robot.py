43500649e34a369a3bcb8faed1dba24200713abd
AreSeven <zay759.xyz@gmail.com>
2023-11-18 12:12:52 +0000

    Removed spaceship setting, Added navigation to team area rather than driving backwards and manual spaceship deposition

--- robot code/robot.py
+++ robot code/robot.py
@@ -2,47 +2,30 @@ from sr.robot3 import *
 
 robot = Robot()
 
-#port is left starboard is right
-#no need to set own spaceship - it is already told by the comp usb
-#therefore this is for use with the contingency (stealing other spaceships)
-
-starship0 = [120, 125]
-starship1 = [121, 126]
-starship2 = [122, 127]
-starship3 = [123, 128]
-starships =[120, 125, 121, 126, 122, 127, 123, 128]
-asteroidsid = [range(150, 199)]
-arenaid = [range(0, 27)]
-eggid = 110
-
-
-
-
-
-
 '''
 print('moving .5 power')
 robot.motor_board.motors[0].power = 0.4
 robot.motor_board.motors[1].power = 0.4
 # sleep for 2 second
-print('sleep 1s')
+print('sleep 0.5s')
 robot.sleep(0.5)
 
 print('stop')
 robot.motor_board.motors[0].power = 0
-robot.motor_board.motors[1].power = 0'''
+robot.motor_board.motors[1].power = 0
 
-'''# roughly x degrees, once we get to image orientation, we'll use a markers yaw to determine how far we have come
+
+#roughly 20 degrees, once we get to image orientation, we'll use a markers yaw to determine how far we have come
 print('turning')
 robot.motor_board.motors[0].power = 0.1
 robot.motor_board.motors[1].power = -0.1
-robot.sleep(1.2)
+robot.sleep(0.5)
 print('stop')
 robot.motor_board.motors[0].power = 0
-robot.motor_board.motors[1].power = 0'''
+robot.motor_board.motors[1].power = 0
+'''
 
-
-# find a marker
+#find a marker
 def firstSee():
     listmarkers = robot.camera.see()
     if len(listmarkers) > 0:
@@ -50,9 +33,30 @@ def firstSee():
         return listmarkers[0]
 
 
-# find a desired target marker and return its information
+asteroidid = [i for i in range(150, 199)]
+print(asteroidid)
 
 
+def firstAsteroid():
+    asteroids = []
+    listmarkers = robot.camera.see()
+    cycle = 0
+    for marker in listmarkers:
+        if marker.id in asteroidid:
+            asteroids.append(marker)
+
+    print(asteroids)
+    #doesnt seem to append at all even when there are asteroids
+    if len(asteroids) > 0:
+        print(f'i see {asteroids[0]} first')
+        return asteroids[0]
+    else:
+        print('i dont see any asteroids')
+        return None
+
+
+
+#find a desired target marker and return its information
 def look(targetid):
     markers = robot.camera.see()
     for marker in markers:
@@ -61,101 +65,252 @@ def look(targetid):
     print(f'couldnt find {targetid}')
     return None
 
+def maincycle():
 
-def setTeamSpaceship():
-    markers = robot.camera.see()
-    spaceships = []
-    for id in markers:
-        if id.id >= 120 and id.id <= 128:
-            spaceships.append(id)
-    if len(spaceships) == 0:
-        print('didnt detect anything')
-        return None
-
-    elif len(spaceships) > 1:
-        print('detected more than one, returning closest')
-        return spaceships[0]
-    else:
-        print(f'team spaceship is: {spaceships}')
-        return spaceships
-
-print(f'i am zone: {robot.zone}')
-myportid = robot.zone + 120
-mystarboard = robot.zone + 125
-print(myportid)
+    mid= robot.zone * 7 + 3
+    firstseen = firstAsteroid()
+    print(firstseen)
+    while firstseen == None:
+        robot.motor_board.motors[0].power = -0.1
+        robot.motor_board.motors[1].power = 0.1
+        robot.sleep(0.1)
+        firstseen = firstAsteroid()
 
 
-firstseen = firstSee()
-print(firstseen)
-while setTeamSpaceship() == None:
-    #setTeamSpaceship()
+    satisfy = False
+    robot.arduino.pins[4].mode = OUTPUT
+    robot.arduino.pins[4].digital_write(True)
+    robot.sleep(0.5)
+    while satisfy == False:
+        if look(firstseen.id) == None:
+            robot.motor_board.motors[0].power = -0.1
+            robot.motor_board.motors[1].power = 0.1
+            robot.sleep(0.1)
+        elif look(firstseen.id).position.horizontal_angle < -0.1:
+            robot.motor_board.motors[0].power = -0.01
+            robot.motor_board.motors[1].power = 0.01
+            robot.sleep(0.001)
+            print(look(firstseen.id).position.horizontal_angle)
+        elif look(firstseen.id).position.horizontal_angle > 0.1:
+            robot.motor_board.motors[0].power = 0.01
+            robot.motor_board.motors[1].power = -0.01
+            robot.sleep(0.001)
+            print(look(firstseen.id).position.horizontal_angle)
+        else:
+            print(look(firstseen.id).position.horizontal_angle)
+            print(f'facing target ({firstseen.id})')
+            robot.motor_board.motors[0].power = 0
+            robot.motor_board.motors[1].power = 0
+            satisfy = True
+    robot.arduino.pins[4].digital_write(False)
+    satisfy = False
+    robot.motor_board.motors[0].power = 0.3
+    robot.motor_board.motors[1].power = 0.3
+
+
+
+    while satisfy == False:
+        moment = look(firstseen.id)
+        if  moment == None:
+            robot.motor_board.motors[0].power = 0
+            robot.motor_board.motors[1].power = 0
+            satisfy = True
+        else:
+            robot.sleep(0.1)
+            if moment.position.horizontal_angle < -0.1:
+                robot.motor_board.motors[0].power = -0.01
+                robot.motor_board.motors[1].power = 0.01
+                robot.sleep(0.01)
+                print(moment.position.horizontal_angle)
+            elif moment.position.horizontal_angle > 0.1:
+                robot.motor_board.motors[0].power = 0.01
+                robot.motor_board.motors[1].power = -0.01
+                robot.sleep(0.01)
+                print(moment.position.horizontal_angle)
+            else:
+                print(moment.position.horizontal_angle)
+                print(f'facing target ({firstseen.id})')
+                robot.motor_board.motors[0].power = 0.3
+                robot.motor_board.motors[1].power = 0.3
+    robot.sleep(0.5)
+    robot.arduino.pins[A4].mode = INPUT
+    distance_to_closest_from_grabber = robot.arduino.pins[A4].analog_read()
+    print(f'{distance_to_closest_from_grabber}m from sensor')
+    #sometimes this reads 5m even though the box is right in front of it :shrug:
+    if distance_to_closest_from_grabber > 0.5:
+        robot.arduino.pins[3].mode = OUTPUT
+        robot.arduino.pins[3].digital_write(True)
+        robot.sleep(0.01)
+
+    #grab code
+    robot.servo_board.servos[0].position = 1
+    robot.servo_board.servos[1].position = 1
+    robot.sleep(1)
+    robot.servo_board.servos[2].position = -0.5
+
+    robot.sleep(2)
+
+    #go home
+    satisfy = False
+    while satisfy == False:
+        moment3 = look(mid)
+        if moment3 == None:
+            robot.motor_board.motors[0].power = -0.1
+            robot.motor_board.motors[1].power = 0.1
+            robot.sleep(0.1)
+            print('dont see')
+        elif moment3.position.horizontal_angle < -0.1:
+            robot.motor_board.motors[0].power = -0.01
+            robot.motor_board.motors[1].power = 0.01
+            robot.sleep(0.01)
+            print(moment3.position.horizontal_angle)
+        elif moment3.position.horizontal_angle > 0.1:
+            robot.motor_board.motors[0].power = 0.01
+            robot.motor_board.motors[1].power = -0.01
+            robot.sleep(0.01)
+            print(moment3.position.horizontal_angle)
+        else:
+            print(moment3.position.horizontal_angle)
+            print(f'facing target ({mid})')
+            midpoint = moment3
+            robot.motor_board.motors[0].power = 0
+            robot.motor_board.motors[1].power = 0
+            satisfy = True
+    robot.arduino.pins[4].digital_write(False)
+    satisfy = False
+    robot.motor_board.motors[0].power = 0.3
+    robot.motor_board.motors[1].power = 0.3
+    while satisfy == False:
+        moment2 = look(mid)
+        if moment2 == None:
+            print('dont see')
+            break
+        elif moment2.position.distance < 500:
+            robot.motor_board.motors[0].power = 0
+            robot.motor_board.motors[1].power = 0
+            satisfy = True
+        else:
+            robot.sleep(0.5)
+            print(f'{moment2.position.distance}mm to mid')
+    robot.sleep(0.5)
+
+    #go to spaceship
+    satisfy = False
+    while satisfy == False:
+        moment5 = look(robot.zone + 120)
+        if moment5 == None:
+            robot.motor_board.motors[0].power = -0.1
+            robot.motor_board.motors[1].power = 0.1
+            robot.sleep(0.1)
+            print('dont see')
+        elif moment5.position.horizontal_angle < -0.1:
+            robot.motor_board.motors[0].power = -0.01
+            robot.motor_board.motors[1].power = 0.01
+            robot.sleep(0.01)
+            print(moment5.position.horizontal_angle)
+        elif moment5.position.horizontal_angle > 0.1:
+            robot.motor_board.motors[0].power = 0.01
+            robot.motor_board.motors[1].power = -0.01
+            robot.sleep(0.01)
+            print(moment5.position.horizontal_angle)
+        else:
+            print(moment5.position.horizontal_angle)
+            print(f'facing target ({robot.zone + 120})')
+            midpoint = moment5
+            robot.motor_board.motors[0].power = 0
+            robot.motor_board.motors[1].power = 0
+            satisfy = True
+    robot.arduino.pins[4].digital_write(False)
+    satisfy = False
+    robot.motor_board.motors[0].power = 0.3
+    robot.motor_board.motors[1].power = 0.3
+    while satisfy == False:
+        moment5 = look(robot.zone + 120)
+        if moment5 == None:
+            print('dont see')
+            break
+        elif moment5.position.distance < 600:
+            robot.motor_board.motors[0].power = 0
+            robot.motor_board.motors[1].power = 0
+            satisfy = True
+        else:
+            if moment5.position.horizontal_angle < -0.1:
+                robot.motor_board.motors[0].power = -0.01
+                robot.motor_board.motors[1].power = 0.01
+                robot.sleep(0.01)
+                print(moment5.position.horizontal_angle)
+            elif moment5.position.horizontal_angle > 0.1:
+                robot.motor_board.motors[0].power = 0.01
+                robot.motor_board.motors[1].power = -0.01
+                robot.sleep(0.01)
+                print(moment5.position.horizontal_angle)
+            robot.sleep(0.1)
+            robot.motor_board.motors[0].power = 0.3
+            robot.motor_board.motors[1].power = 0.3
+            print(f'{moment5.position.distance}mm to {moment5.id}')
+    robot.sleep(0.2)
+    print('raising')
+    robot.servo_board.servos[2].position = 1
+    robot.sleep(2)
+    robot.motor_board.motors[0].power = 0.2
+    robot.motor_board.motors[1].power = 0.2
+    robot.sleep(1.7)
+    robot.motor_board.motors[0].power = 0
+    robot.motor_board.motors[1].power = 0
+    robot.servo_board.servos[0].position = -1
+    robot.servo_board.servos[1].position = -1
+    robot.sleep(1)
+    robot.motor_board.motors[0].power = -0.3
+    robot.motor_board.motors[1].power = -0.3
+    robot.sleep(1)
+    robot.motor_board.motors[0].power = 0
+    robot.motor_board.motors[1].power = 0
+    robot.servo_board.servos[2].position = -1
+    print('turning')
+    robot.motor_board.motors[0].power = -0.1
+    robot.motor_board.motors[1].power = 0.1
+    robot.sleep(0.9)
+
+
+
+
+    '''print('moving 0.4 power')
+    robot.motor_board.motors[0].power = 0.4
+    robot.motor_board.motors[1].power = 0.4
+    robot.sleep(0.5)
+    robot.motor_board.motors[0].power = 0
+    robot.motor_board.motors[1].power = 0
     print('turning')
     robot.motor_board.motors[0].power = 0.1
     robot.motor_board.motors[1].power = -0.1
-    robot.sleep(0.2)
+    robot.sleep(0.9)
+    robot.motor_board.motors[0].power = 0
+    robot.motor_board.motors[1].power = 0
+    print('releasing')
+    robot.servo_board.servos[2].position = -1
+    robot.sleep(2)
+    robot.servo_board.servos[0].position = -1
+    robot.servo_board.servos[1].position = -1
+    robot.sleep(1)
+    print('moving -0.4 power')
+    robot.motor_board.motors[0].power = -0.4
+    robot.motor_board.motors[1].power = -0.4
+    robot.sleep(0.5)
+    robot.motor_board.motors[0].power = 0
+    robot.motor_board.motors[1].power = 0
+    print('turning')
+    robot.motor_board.motors[0].power = -0.1
+    robot.motor_board.motors[1].power = 0.1
+    robot.sleep(0.9)
+    robot.motor_board.motors[0].power = 0
+    robot.motor_board.motors[1].power = 0
+    print('moving 0.4 power')
+    robot.motor_board.motors[0].power = 0.4
+    robot.motor_board.motors[1].power = 0.4
+    robot.sleep(1)
+    robot.motor_board.motors[0].power = 0
+    robot.motor_board.motors[1].power = 0'''
 
-'''
-satisfy = False
-robot.arduino.pins[4].mode = OUTPUT
-robot.arduino.pins[4].digital_write(True)
-robot.sleep(0.5)
-while satisfy == False:
-    if look(firstseen.id) == None:
-        robot.motor_board.motors[0].power = -0.01
-        robot.motor_board.motors[1].power = 0.01
-        robot.sleep(0.01)
-    elif look(firstseen.id).position.horizontal_angle > 0.05 or look(firstseen.id).position.horizontal_angle < -0.05:
-        robot.motor_board.motors[0].power = -0.01
-        robot.motor_board.motors[1].power = 0.01
-        robot.sleep(0.01)
-    else:
-        print(look(firstseen.id).position.horizontal_angle)
-        print(f'facing target ({firstseen.id})')
-        robot.motor_board.motors[0].power = 0
-        robot.motor_board.motors[1].power = 0
-        satisfy = True
-robot.arduino.pins[4].digital_write(False)
-satisfy = False
-robot.motor_board.motors[0].power = 0.3
-robot.motor_board.motors[1].power = 0.3
-
-while satisfy == False:
-    if look(firstseen.id) == None:
-        robot.motor_board.motors[0].power = 0
-        robot.motor_board.motors[1].power = 0
-        satisfy = True
-    else:
-        robot.sleep(0.5)
-robot.sleep(0.5)
-robot.arduino.pins[A4].mode = INPUT
-distance_to_closest_from_grabber = robot.arduino.pins[A4].analog_read()
-print(f'{distance_to_closest_from_grabber}m from sensor')
-# sometimes this reads 5m even though the box is right in front of it :shrug:
-if distance_to_closest_from_grabber > 0.5:
-    robot.arduino.pins[3].mode = OUTPUT
-    robot.arduino.pins[3].digital_write(True)
-    robot.sleep(0.01)
-
-# grab code
-robot.servo_board.servos[0].position = 1
-robot.servo_board.servos[1].position = 1
-robot.sleep(1)
-robot.servo_board.servos[2].position = 1
-
-robot.sleep(2)
-
-print('moving -0.4 power')
-robot.motor_board.motors[0].power = -0.4
-robot.motor_board.motors[1].power = -0.4
-robot.sleep(0.01)
 
 while True:
-    robot.arduino.pins[2].mode = INPUT
-    hurt_bum = robot.arduino.pins[2].digital_read()
-    robot.sleep(0.01)
-    if hurt_bum == True:
-        print('ow')
-        robot.motor_board.motors[0].power = 0
-        robot.motor_board.motors[1].power = 0
-        robot.sleep(0.01)
-'''
+    maincycle()
