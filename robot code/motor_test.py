ed16644d6ccedac42fc6d6368a25333d0762b482
Umar <108488645+umar-9@users.noreply.github.com>
2023-12-12 11:13:53 +0000

    Added print statements and also testing motor current
    Motor current will be useful later but still worth testing now while we can

--- robot code/motor_test.py
+++ robot code/motor_test.py
@@ -7,22 +7,29 @@ def brake():
     mb[0].power = 0
     mb[1].power = 0
 
-
+print("Moving forwards")
 mb.motors[0].power = 0.4
 mb.motors[1].power = 0.4
 
 robot.sleep(5)
+print("Braking")
 brake()
 robot.sleep(1)
 
+print("Moving backwards")
 mb.motors[0].power = -0.4
 mb.motors[1].power = -0.4
 
 robot.sleep(5)
+print("Braking")
 brake()
 robot.sleep(1)
-
+print("Turning")
 mb.motors[0].power = 0.4
 mb.motors[1].power = -0.4
 
-robot.sleep(1)
+robot.sleep(5)
+
+
+# Useful to test motor current (not needed for challenges) but will probably need later with the actual robot.
+print("Motor currents: ", mb.motors[0].current, mb.motors[1].current)
