910f4dc8527510549bb055f9d73852186913ee1e
Kingshuk Hossain <kingshukhossain@outlook.com>
2024-01-02 12:37:39 +0000

    Added without encoder movement challenge code

--- robot code/movement challenge - without encoder.py
+++ robot code/movement challenge - without encoder.py
@@ -1,2 +1,35 @@
 from sr.robot3 import *
 robot = Robot()
+mts = robot.motor_board.motors
+
+def move():
+    mts[0].power = 1
+    mts[1].power = 1
+
+def brake():
+    mts[0].power = 0
+    mts[1].power = 0
+
+def turn():
+    mts[0].power = -1
+    mts[1].power = 1
+
+while True:
+    move()
+    robot.sleep(7)
+    brake()
+    robot.sleep(0.1)
+
+    turn()
+    robot.sleep(2)
+
+    brake()
+    robot.sleep(0.1)
+    move()
+    robot.sleep(10)
+    brake()
+    robot.sleep(0.1)
+
+
+
+    
