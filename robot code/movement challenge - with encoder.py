8af3bca9208daadd710be63211b816ed7ed4126e
AreSeven <zay759.xyz@gmail.com>
2024-01-10 17:29:41 +0000

    updated encoder code from during lesson for debugging (print statements)

--- robot code/movement challenge - with encoder.py
+++ robot code/movement challenge - with encoder.py
@@ -5,6 +5,7 @@ robot = Robot()
 mts = robot.motor_board.motors
 pins = robot.arduino.pins
 
+
 ENCODER_CYCLE = (2 * math.pi) / 11
 pins[2].mode = INPUT
 pins[3].mode = INPUT
@@ -16,58 +17,68 @@ sequence = [
     "11"
 ]
 
-on_short_side = True
 
-while True:
-    turning = False
+def encoderLoop():
+    on_short_side = True
 
-    # With 80mm wheels, there are 4 full cycles in 1000mm, and 6 full cycles in 1500mm
-    cycles = 0
+    while True:
+        turning = False
+
+        # With 80mm wheels, there are 4 full cycles in 1000mm, and 6 full cycles in 1500mm
+        cycles = 0
 
 
-    # Reads initial encoder position as a binary string
-    prev_seq = str(int(pins[2].digital_read())) + str(int(pins[3].digital_read()))
-    seq_pos = sequence.find(prev_seq)
-    degrees_rotated = 0
+        # Reads initial encoder position as a binary string
+        prev_seq = str(int(pins[2].digital_read())) + str(int(pins[3].digital_read()))
+        seq_pos = sequence.index(prev_seq)
+        degrees_rotated = 0
 
-    while not turning:
-        mts[0].power = 0.7
-        mts[1].power = 0.7
+        while not turning:
+            mts[0].power = 0.1
+            mts[1].power = 0.1
 
-        encoderA = str(int(pins[2].digital_read()))
-        encoderB = str(int(pins[3].digital_read()))
-        bin_str = encoderA + encoderB
-        str_idx = sequence.find(bin_str)
+            encoderA = str(int(pins[2].digital_read()))
+            encoderB = str(int(pins[3].digital_read()))
+            bin_str = encoderA + encoderB
+            str_idx = sequence.index(bin_str)
+            print(f"Current encoder: {bin_str}\n")#Prev encoder: {prev_seq}")
 
-        # Checks to see if the new binary string read follows the previous reading in the sequence, going clockwise
-        if (prev_seq != bin_str) and (str_idx == (seq_pos + 1) % 4):
-            degrees_rotated += ENCODER_CYCLE 
-            seq_pos += 1
-            seq_pos = seq_pos % 4
-            prev_seq = bin_str
-        
-        if degrees_rotated == 2 * math.pi:
-            cycles += 1
-        
-        if cycles == 4 and on_short_side:
-            on_short_side = False
-            turning = True
-        elif cycles == 6 and not on_short_side:
-            on_short_side = True
-            turning = True
-    
-    mts[0].power = 0
-    mts[1].power = 0
-    robot.sleep(0.2)
+            # Checks to see if the new binary string read follows the previous reading in the sequence, going clockwise
+            if str_idx == -1:
+                print("String not found in sequence‼️‼️")
+            elif (prev_seq != bin_str) and ((str_idx == (seq_pos + 1) % 4) or (str_idx == (seq_pos - 1) % 4)):
+                degrees_rotated += ENCODER_CYCLE
+                seq_pos += 1
+                seq_pos = seq_pos % 4
+                prev_seq = bin_str
 
-    # Robot turns left (?)
-    # Needs adjustment
-    mts[0].power = -0.2
-    mts[1].power = 0.2
-    robot.sleep(3)
-    
 
-        
+            if degrees_rotated == 2 * math.pi:
+                cycles += 1
+                print(f'1{cycles} cycles')
+                print(f'{degrees_rotated} degrees rotated')
+
+            if cycles == 4 and on_short_side:
+                on_short_side = False
+                turning = True
+            elif cycles == 6 and not on_short_side:
+                on_short_side = True
+                turning = True
+
+            robot.sleep(0.1)
+
+        mts[0].power = 0
+        mts[1].power = 0
+        robot.sleep(0.2)
+
+        # Robot turns left (?)
+        # Needs adjustment
+        mts[0].power = -0.2
+        mts[1].power = 0.2
+        robot.sleep(3)
+
+
+encoderLoop()
 
 
 
