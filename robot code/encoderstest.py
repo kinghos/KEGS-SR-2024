3d85c86130bdb77fba1b0db64de27c3b65506958
Umar <108488645+umar-9@users.noreply.github.com>
2024-01-16 10:06:29 +0000

    Update encoderstest.py

--- robot code/encoderstest.py
+++ robot code/encoderstest.py
@@ -6,12 +6,12 @@ mts = robot.motor_board.motors
 pins = robot.arduino.pins
 
 
-# ENCODER_CYCLE = (0.5*math.pi) / (34) - closest one yet (around 2 cycles out at 20 cycles)
-# ENCODER_CYCLE = (0.25*math.pi) / (2*11) # - seems to be pretty much bang  (maybe 2-3 cycles too high) at 40 cycles)
+# ENCODER_CYCLE = (0.5*math.pi) / (34) - around 2 cycles out at 20 cycles
+# ENCODER_CYCLE = (0.25*math.pi) / (2*11) # - 2-3 cycles too high at 40 cycles
 # ENCODER_CYCLE = (10*math.pi) / (2*374) # - drifts around 2-3 cycles out (too low) at 30 cycles
 # ENCODER_CYCLE = (math.pi) / (2*34) # drifts around 5 cycles (too high) at 30 cycles
 
-ENCODER_CYCLE = (4*math.pi) / (374)
+ENCODER_CYCLE = (4*math.pi) / (374) # starts off 1 higher, but then at like 20 cycles, it's perfect
 
 pins[2].mode = INPUT
 pins[3].mode = INPUT
