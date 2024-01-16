ce31fd9a0ae5bc626f1030f922951dfcdf89a76b
Umar <108488645+umar-9@users.noreply.github.com>
2024-01-16 23:17:29 +0000

    Update encoderstest.py

--- robot code/encoderstest.py
+++ robot code/encoderstest.py
@@ -6,12 +6,12 @@ mts = robot.motor_board.motors
 pins = robot.arduino.pins
 
 
+# WRONG VALUES
 # ENCODER_CYCLE = (0.5*math.pi) / (34) - around 2 cycles out at 20 cycles
 # ENCODER_CYCLE = (0.25*math.pi) / (2*11) # - 2-3 cycles too high at 40 cycles
 # ENCODER_CYCLE = (10*math.pi) / (2*374) # - drifts around 2-3 cycles out (too low) at 30 cycles
 # ENCODER_CYCLE = (math.pi) / (2*34) # drifts around 5 cycles (too high) at 30 cycles
-
-ENCODER_CYCLE = (4*math.pi) / (374) # starts off 1 higher, but then at like 20 cycles, it's perfect
+# ENCODER_CYCLE = (4*math.pi) / (374) # starts off 1 higher, but then at like 20 cycles, it's perfect
 
 pins[2].mode = INPUT
 pins[3].mode = INPUT
@@ -32,6 +32,7 @@ def encoderLoop():
     prev_seq = encoderA + encoderB
     seq_pos = sequence.index(prev_seq)
     cycles = 0
+    counts = 0
     rad_rotated = 0
     while True:
         encoderA = str(int(pins[2].digital_read()))
@@ -40,6 +41,10 @@ def encoderLoop():
         str_idx = sequence.index(bin_str)
 
         rad_rotated += ENCODER_CYCLE * abs(str_idx - seq_pos)
+        counts += abx(str_idx - seq_pos)
+        print(counts)
+        seq_pos = str_idx
+        prev_seq = bin_str
         #print(rad_rotated)
         if rad_rotated >= 2*math.pi:
             cycles += 1
