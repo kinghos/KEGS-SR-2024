49e7d27c809df1db7cd4e5a30e31f4d1364072aa
Umar <108488645+umar-9@users.noreply.github.com>
2024-01-31 13:22:01 +0000

    Update motor_test.py

--- robot code/motor_test.py
+++ robot code/motor_test.py
@@ -11,25 +11,5 @@ print("Moving forwards")
 mb[0].power = 0.4
 mb[1].power = 0.4
 
-robot.sleep(5)
-print("Braking")
-brake()
-robot.sleep(1)
-
-print("Moving backwards")
-mb[0].power = -0.4
-mb[1].power = -0.4
-
-robot.sleep(5)
-print("Braking")
-brake()
-robot.sleep(1)
-print("Turning")
-mb[0].power = 0.4
-mb[1].power = -0.4
-
-robot.sleep(5)
-
-
-# Useful to test motor current (not needed for challenges) but will probably need later with the actual robot.
-print("Motor currents: ", mb[0].current, mb[1].current)
+while True:
+    pass
