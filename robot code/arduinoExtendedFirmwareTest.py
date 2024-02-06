36c0e99348b8d8ec0c164d72beedac48f611dd40
Kingshuk Hossain <kingshukhossain@outlook.com>
2024-02-06 22:47:41 +0000

    FINAL FIX PLEASE!!

--- robot code/arduinoExtendedFirmwareTest.py
+++ robot code/arduinoExtendedFirmwareTest.py
@@ -1,17 +1,12 @@
 from sr.robot3 import *
-import threading
 
 robot = Robot()
 mts = robot.motor_board.motors
 arduino = robot.arduino
 
-
-def readDistance():
-    distance = arduino.command("e") # Uses 'e' command to read distance
-
-
 while True:
     mts[0].power = 0.1
     mts[1].power = 0.1
-    distance = readDistance()
+    distance = arduino.command("e") # Uses 'e' command to read distance
     print(distance) # Prints cumulative distance
+    robot.sleep(0.1)
