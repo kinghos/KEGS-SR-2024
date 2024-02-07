db4ca87e3ae288adb536dd724e3c27c0c7d899de
umar-9 <umarsultonov21@gmail.com>
2024-02-07 16:24:31 +0000

    update

--- Challenges/movement challenge/old encoder wo arduino.py
+++ Challenges/movement challenge/old encoder wo arduino.py
@@ -6,7 +6,7 @@ mts = robot.motor_board.motors
 pins = robot.arduino.pins
 
 
-ENCODER_CYCLE = (2*math.pi) / (4*374)
+ENCODER_CYCLE = (2*math.pi) / (4*11) 
 pins[2].mode = INPUT
 pins[3].mode = INPUT
 
