c57e70fddeb0ef088f83cee20ddfab8e05fe59f3
Umar <108488645+umar-9@users.noreply.github.com>
2024-02-08 21:44:15 +0000

    basicEncoderTest.py: Moved motor power outside while True

--- robot code/basicEncoderTest.py
+++ robot code/basicEncoderTest.py
@@ -9,9 +9,9 @@ CPR = 2 * math.pi / (4 * 11)
 WHEEL_DIAMETER = 80
 encoderCount = 0
 
+mts[0].power = 0.25
+mts[1].power = 0.25
 while True:
-    mts[0].power = 0.25
-    mts[1].power = 0.25
     encoderCount = int(arduino.command("v"))
     distance = (encoderCount / CPR) * math.pi * WHEEL_DIAMETER
     print(f"Count: {encoderCount},\t Distance: {distance}mm")
