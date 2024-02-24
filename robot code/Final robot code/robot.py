ec3c5feb7a7a2b25d3c6afd57675ba451ae87a0f
Kingshuk Hossain <kingshukhossain@outlook.com>
2024-02-24 23:29:59 +0000

    Pseudocode

--- robot code/Final robot code/robot.py
+++ robot code/Final robot code/robot.py
@@ -9,6 +9,31 @@ uno = robot.arduino
 CPR = 2 * math.pi / (4 * 11)
 WHEEL_DIAMETER = 80
 
-def main():
+def brake():
+    motors[0].power = 0
+    motors[1].power = 0
+
+def findMarker(marker):
     pass
 
+def mechanismGrab():
+    pass
+
+def mechanismRelease():
+    pass
+
+def approachAsteroid(marker):
+    findMarker(marker)
+
+def approachBase():
+    pass
+
+def main():
+    approachAsteroid()
+    mechanismGrab()
+    approachBase()
+    mechanismRelease()
+    
+    # check if egg in base
+
+    
