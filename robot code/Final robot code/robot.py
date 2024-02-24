ed24562e64d5f7e96af63a5416250faa11011bbc
Kingshuk Hossain <kingshukhossain@outlook.com>
2024-02-24 22:33:37 +0000

    Added final robot code

---
+++ robot code/Final robot code/robot.py
@@ -0,0 +1,14 @@
+# Draft started 24/2/24
+
+from sr.robot3 import *
+
+robot = Robot()
+motors = robot.motor_board.motors
+uno = robot.arduino
+
+CPR = 2 * math.pi / (4 * 11)
+WHEEL_DIAMETER = 80
+
+def main():
+    pass
+
