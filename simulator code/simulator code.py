758bee0ffaaaf68f3969b1b11c620772cc20b8e9
umar-9 <umarsultonov21@gmail.com>
2024-02-27 23:48:25 +0000

    NOTWORKINGYET Code refactoring, increased efficiency/speed, added more -1 & timesup contingencies

--- simulator code/simulator code.py
+++ simulator code/simulator code.py
@@ -23,6 +23,12 @@ def brake():
     robot.motor_board.motors[0].power = 0
     robot.motor_board.motors[1].power = 0
 
+# drive fast
+# TO DO: RAMP UP SPEED GRADUALLY WHERE THIS FUNCTION IS USED TO PREVENT TIPPING 
+    # (it doesn't tip over yet but would tip if bumped)
+def fastDrive():
+    robot.motor_board.motors[0].power = 0.8
+    robot.motor_board.motors[1].power = 0.8
 
 # drive at a stable pace
 def mediumDrive():
@@ -39,21 +45,21 @@ def backwardsDrive(speed):
 # slow turning, clockwise is true, counter clockwise is false
 def slowTurn(clockwise: bool):
     if clockwise == True:
-        robot.motor_board.motors[0].power = 0.05
-        robot.motor_board.motors[1].power = -0.05
+        robot.motor_board.motors[0].power = 0.25
+        robot.motor_board.motors[1].power = -0.25
     else:
-        robot.motor_board.motors[0].power = -0.05
-        robot.motor_board.motors[1].power = 0.05
+        robot.motor_board.motors[0].power = -0.25
+        robot.motor_board.motors[1].power = 0.25
 
 
 # fast turning, clockwise is true, counter clockwise is false
 def fastTurn(clockwise: bool):
     if clockwise == True:
-        robot.motor_board.motors[0].power = 0.1
-        robot.motor_board.motors[1].power = -0.1
+        robot.motor_board.motors[0].power = 0.5
+        robot.motor_board.motors[1].power = -0.5
     else:
-        robot.motor_board.motors[0].power = -0.1
-        robot.motor_board.motors[1].power = 0.1
+        robot.motor_board.motors[0].power = -0.5
+        robot.motor_board.motors[1].power = 0.5
 
 
 # find a desired target marker and return its information
@@ -66,18 +72,34 @@ def look(targetid):
     return None
 
 
+# Returns if facing target
+def accurateTurn(target):
+    if target.position.horizontal_angle < -0.5:
+        fastTurn(False)
+        print(target.position.horizontal_angle)
+    elif target.position.horizontal_angle > 0.5:
+        fastTurn(False)
+        print(target.position.horizontal_angle)
+    elif target.position.horizontal_angle < -0.1:
+        slowTurn(False)
+        print(target.position.horizontal_angle)
+    elif target.position.horizontal_angle > 0.1:
+        slowTurn(True)
+        print(target.position.horizontal_angle)
+    else:
+        return True
+    return False
+
+
 """
+    turn counter-clockwise until in line with target, slow turn to a degree of accuracy
     target argument may be a list or a integer target id
 
     returns -1 for 'times up'
     returns marker info for success
 """
-
-
-# turn counter-clockwise until in line with target, slow turn to a degree of accuracy
 def turnSee(target, direction=False):
-    if isinstance(target,
-                  int):  # If the argument passsed is an integer, convert to a list (this conversion allows for amalgamation of previous turnSee and turnSeeList)
+    if isinstance(target, int):  # If the argument passsed is an integer, convert to a list (this conversion allows for amalgamation of previous turnSee and turnSeeList)
         target = [target, ]
 
     facing_target = False
@@ -102,31 +124,16 @@ def turnSee(target, direction=False):
             print(f'turnSee could not find {target}')
             continue
 
-        # next 2 elifs are for turning until within a certain angle of accuracy
-        if looktarget.position.horizontal_angle < -0.5:
-            fastTurn(False)
-            robot.sleep(0.05)
-            print(looktarget.position.horizontal_angle)
-        elif looktarget.position.horizontal_angle > 0.5:
-            fastTurn(False)
-            robot.sleep(0.05)
-            print(looktarget.position.horizontal_angle)
-        elif looktarget.position.horizontal_angle < -0.1:
-            slowTurn(False)
-            robot.sleep(0.05)
-            print(looktarget.position.horizontal_angle)
-        elif looktarget.position.horizontal_angle > 0.1:
-            slowTurn(True)
-            robot.sleep(0.05)
-            print(looktarget.position.horizontal_angle)
-        # is now facing the target, stop turning and end loop
-        else:
-            print(f'facing target ({target}) \t horiz angle: {looktarget.position.horizontal_angle}')
+        # Accurately turn to target. If facing, stop turning & end loop
+        if accurateTurn(looktarget):
             brake()
             facing_target = True
+    print(f'facing target ({target}) \t horiz angle: {looktarget.position.horizontal_angle}')
     return looktarget
 
 
+# TO DO - TEST & FIX
+# ⇒ True/False return
 def isAsteroidRetrievable(marker):
     marker_camera_vertical_distance = marker.position.distance * math.sin(marker.position.vertical_angle)
     # If negative, then the marker lies below the camera
@@ -139,20 +146,22 @@ def isAsteroidRetrievable(marker):
 """
   Returns closest marker and the direction the robot is currently turning to scan the arena
    => return (closest, clockwise_turn)
+   ⇒ returns None if it can't find anything or if times up
 """
-
-
 def closestAsteroid():
     seen_opposite_left_id = False  # If starting at zone 0, ids in question are 13 and 14
     seen_opposite_right_id = False  # If starting at zone 0, id in question are 20 and 21 (consider 2 for contingency)
     seen_base_middle_id = False  # Added contingency
     asteroids = []
     clockwise_turn = False  # which way to turn for scanning of arena
+    tempTime = robot.time()
 
     while not ((seen_opposite_left_id and seen_opposite_right_id) or seen_base_middle_id):
-        print(f'{str(seen_opposite_left_id)} {str(seen_opposite_right_id)} {str(seen_base_middle_id)}')
-        print('boo')
-
+        
+        if (robot.time() - tempTime) > 5:
+            print('times up')
+            return None
+        
         if seen_opposite_left_id and not seen_opposite_right_id:
             clockwise_turn = True
         elif seen_opposite_right_id and not seen_opposite_left_id:
@@ -191,6 +200,7 @@ def closestAsteroid():
     return (closest, clockwise_turn)
 
 
+# ⇒ No return value
 def untilUnsee(target_id):
     print('untilunsee')
     lost_sight_of_target = False
@@ -202,26 +212,18 @@ def untilUnsee(target_id):
             robot.sleep(0.1)
             brake()
             lost_sight_of_target = True
-        else:
-            # keep moving
-            robot.sleep(0.1)
-            # course correction
-            if moment.position.horizontal_angle < -0.1:
-                slowTurn(False)
-                robot.sleep(0.01)
-                print(moment.position.horizontal_angle)
-            elif moment.position.horizontal_angle > 0.1:
-                slowTurn(True)
-                robot.sleep(0.01)
-                print(moment.position.horizontal_angle)
+        elif accurateTurn(moment): # Course correction
+            if moment.position.distance > 500:
+                fastDrive()
             else:
-                # keep going at 0.3 power and print current angle
-                print(f'facing target ({moment.id}) \t horiz angle: {moment.position.horizontal_angle}')
                 mediumDrive()
 
 
+# Returns -1 for a fail caused by targetid disappearing
+# Resets if times up
 def correctDrive(targetid, distance):
     arrived_at_target = False
+    robot.sleep(0.01)
     tempTime = robot.time()
     while arrived_at_target == False:
         if (robot.time() - tempTime) > 10:
@@ -231,41 +233,39 @@ def correctDrive(targetid, distance):
             robot.sleep(0.9)
             brake()
             reset()
+            return
         # set target marker (ship) information to temp variable, in case it cannot see it later
         target = look(targetid)
         # if it doesnt see the marker than just break out, better to muck up once than to have an error and just obliterate the whole robot
         if target == None:
             print('correctDrive can\'t find target marker')
             brake()
-            turnSee(targetid)
+            if turnSee(targetid) == -1:
+                print(f"{targetid} disappeared?")
+                return -1
         # stop if it gets close to the target
         elif target.position.distance < distance:
             brake()
             arrived_at_target = True
-        else:
+        elif accurateTurn(target):
             # course correction
-            if target.position.horizontal_angle < -0.1:
-                slowTurn(False)
-                robot.sleep(0.01)
-                print(target.position.horizontal_angle)
-            elif target.position.horizontal_angle > 0.1:
-                slowTurn(True)
-                robot.sleep(0.01)
-                print(target.position.horizontal_angle)
+            robot.sleep(0.1)
+            print(f'{target.position.distance}mm to {target.id}')
+            if target.position.distance > 2*distance:
+                fastDrive()
             else:
-                # drive at 0.3 power and print distance to marker
-                robot.sleep(0.1)
                 mediumDrive()
-                print(f'{target.position.distance}mm to {target.id}')
 
 
+# ⇒ Returns -1 if correctdrive fails
 def spaceshipDeposit(spaceship_id):
     print('going to spaceship')
 
     # drive forward
     mediumDrive()
 
-    correctDrive(spaceship_id, 600)
+    if correctDrive(spaceship_id, 600) == -1:
+        return -1
     print('finished correct driving')
 
     robot.sleep(0.2)
@@ -280,11 +280,11 @@ def spaceshipDeposit(spaceship_id):
     robot.motor_board.motors[0].power = 0.2
     robot.motor_board.motors[1].power = 0.2
 
-    robot.sleep(1.6)
+    robot.sleep(1.4)
 
     brake()
     global collected
-    # effecient stacking
+    # efficient stacking
     print(f'i have collected {collected} asteroids so far')
     if collected % 2 == 0:
         print('depositing to 1')
@@ -312,32 +312,45 @@ def spaceshipDeposit(spaceship_id):
     robot.servo_board.servos[2].position = -0.2
 
 
+# ⇒ Returns -1 for failure (can't find the base markers)
 def planetDeposit():
     print("going to planet for deposition")
 
     for i in range(3, 7):
         if turnSee(BASE_IDS[i], True) != -1:
             correctDrive(BASE_IDS[i], 500)
-            break
+            brake()
+            drop()
+            return
+        
+    return -1
 
-    brake()
 
-    drop()
-
-
-# Returns a list of distances between the base markers and a target marker
+# TO DO: TEST & FIX
+# Finds distances between the base markers and a target marker
+# ⇒ returns -1 for times up
 def baseMarkerDistanceFinder(target_marker):
     marker_target_distances = []
-    for base_id in BASE_IDS:
-        base_marker = look(base_id)
-        if base_marker == None:
-            continue
-        distance_to_base_id = base_marker.position.distance
-        marker_target_distances.append(abs(distance_to_base_id - target_marker.position.distance))
-    return marker_target_distances
+    base_marker_found = False
+    tempTime = robot.time()
+    while not base_marker_found:
+        if (robot.time() - tempTime) > 10:
+            print('times up')
+            return -1
+        turnSee(BASE_IDS)
+        for base_id in BASE_IDS:
+            base_marker = look(base_id)
+            if base_marker == None:
+                continue
+            base_marker_found = True
+            print(f"BASE MARKER CALCULATED FOR: {base_id}")
+            distance_to_base_id = base_marker.position.distance
+            marker_target_distances.append(abs(distance_to_base_id - target_marker.position.distance))
+        return marker_target_distances
 
 
-# TO DO
+# Moves egg to the nearest enemy base
+# ⇒ No return value
 def eggMover(direction_of_turn):
     print("Moving egg out of base")
     mediumDrive()
@@ -354,7 +367,12 @@ def eggMover(direction_of_turn):
     robot.sleep(2)
     brake()
 
-    turnSee(EGG_ID, not direction_of_turn)
+    if turnSee(EGG_ID, not direction_of_turn) == -1:
+        backwardsDrive(0.4)
+        robot.sleep(1.5)
+        brake()
+        eggChecker()
+        return
     # move forward
     mediumDrive()
     # drive forward until no longer able to see asteroid
@@ -390,16 +408,13 @@ def eggMover(direction_of_turn):
     robot.sleep(0.9)
     brake()
 
-    #reset()
-    return
+    reset()
 
 
 """
   Returns whether egg is arena and the direction the robot is currently turning to scan the base
    => return (is_egg_in_base, clockwise_turn)
 """
-
-
 def eggChecker():
     seen_base_left_id = False  # If starting at zone 0, ids in question are 27, 0, 1 (consider 3 for extra contingency)
     seen_base_right_id = False  # If starting at zone 0, id in question are 5, 6, 7 (consider 3 for extra contingency)
@@ -431,14 +446,21 @@ def eggChecker():
 
 def reset():
     maincycle()
+    return
 
 
 def grab():
-    # lowering forklift here
-    robot.servo_board.servos[2].position = -1
-    robot.sleep(0.7)
+    # Lowering forklift and grabbing halfway at the same time for efficiency
+    robot.servo_board.servos[2].position = -1 #lower
+    robot.servo_board.servos[0].position = 0.6 #grab
+    robot.servo_board.servos[1].position = 0.6 #grab
+    robot.sleep(0.3)
+    robot.servo_board.servos[0].position = 0
+    robot.servo_board.servos[1].position = 0
+    robot.servo_board.servos[2].position = -1 #lower
+    robot.sleep(0.4)
 
-    # read distance to sensor using s o n a r woah
+    # read distance to sensor
     robot.arduino.pins[A4].mode = INPUT
     distance_to_closest_from_grabber = robot.arduino.pins[A4].analog_read()
 
@@ -453,7 +475,7 @@ def grab():
     # grab box
     robot.servo_board.servos[0].position = 0.6
     robot.servo_board.servos[1].position = 0.6
-    robot.sleep(0.6)
+    robot.sleep(0.3)
 
     # lift up with forklift a bit
     robot.servo_board.servos[2].position = -0.8
@@ -473,7 +495,7 @@ def drop():
     brake()
     robot.servo_board.servos[2].position = -0.2
 
-
+# TO DO: Figure out why it's saying egg in base (when it isn't) AFTER when its calculating spaceship distances
 # choose asteroid, go to asteroid, go to base, go to spaceship, put asteroid in spaceship, repeat
 def maincycle():
     # lift up the forklift a bit to ensure no collision with raised platform/other boxes
@@ -484,28 +506,29 @@ def maincycle():
 
     # set a target asteroid to pick up
     asteroid_info = closestAsteroid()
-    firstasteroid = asteroid_info[0]
-
-    asteroid_direction_of_turn = not asteroid_info[1]
 
     # if it didnt see an asteroid, it will turn counter-clockwise until it does and set that as its target
-    while firstasteroid == None:
+    while asteroid_info == None:
         print('I did not see an asteroid to target')
         slowTurn(False)
         robot.sleep(0.1)
         asteroid_info = closestAsteroid()
-        firstasteroid = asteroid_info[0]
-        asteroid_direction_of_turn = not asteroid_info[
-            1]  # if we turned CW to scan the arena, we turn CCW to find the asteroid
-    print(
-        f'I have set {firstasteroid.id} as the target asteroid. To reach this asteroid I will turn clockwise = {asteroid_direction_of_turn}')
+
+    firstasteroid = asteroid_info[0]
+    asteroid_direction_of_turn = not asteroid_info[1]  # if we turned CW to scan the arena, we turn CCW to find the asteroid
+
+    print(f'I have set {firstasteroid.id} as the target asteroid. To reach this asteroid I will turn clockwise = {asteroid_direction_of_turn}')
     print(f'The target asteroid has these stats: {firstasteroid}')
 
     #add current asteroid to list of blacklisted asteroids so that robot doesnt target already retrieved asteroids such as those in the spaceship
     blackList.append(firstasteroid.id)
 
     # turn until it is in line with the target asteroid
-    turnSee(firstasteroid.id, asteroid_direction_of_turn)
+    can_see_asteroid = turnSee(firstasteroid.id, asteroid_direction_of_turn)
+    if can_see_asteroid == -1:
+        print("Lost asteroid sight")
+        reset()
+        return
 
     # move forward
     mediumDrive()
@@ -524,46 +547,70 @@ def maincycle():
 
 
     seeLeftBase = turnSee(BASE_IDS[2], egg_direction_of_turn)
-    if seeLeftBase == -1:
-        seeMid = turnSee(BASE_IDS)
-        if seeMid != -1:
-            correctDrive(seeMid.id, 500)
-    else:
+    if seeLeftBase == -1: # If it can't see base marker 2
+        see_first_base = turnSee(BASE_IDS) # Then find the first base marker it does see
+        if see_first_base != -1: # If it has been found, drive to it
+            if correctDrive(see_first_base.id, 500) == -1: # If driving fails, reset
+                drop()
+                reset()
+                return
+        else: # If no base markers are visible
+            drop()
+            reset()
+            return
+    else: # If base marker 2 found, drive to it
         print(f'going to {BASE_IDS[2]} (base)')
-        correctDrive(BASE_IDS[2], 500)
+        if correctDrive(BASE_IDS[2], 500) == -1: # If driving fails, reset
+            drop()
+            reset()
+            return
 
 
     robot.sleep(0.2)
 
     # go to spaceship
     seeSpaceship = turnSee([PORT_ID, STARBOARD_ID], False)
-    if seeSpaceship != -1:
+    if seeSpaceship != -1: # If spaceship has been found
         spaceship_marker = look(seeSpaceship.id)
-    else:
-        print('depositing in planet')
-        planetDeposit()
-
+    else: # If spaceship not found
+        if planetDeposit() == -1:
+            reset()
+            return
         print('turning')
         fastTurn(False)
 
         robot.sleep(0.9)
         reset()
+        return
 
-    '''# For each base marker seen, calculate distance between the base marker and the port marker. 
+    # For each base marker seen, calculate distance between the base marker and the port marker. 
     # If all of these distances are over 300, then the spaceship is considered out of our base
     marker_spaceship_distances = baseMarkerDistanceFinder(spaceship_marker)
-    print(f'spaceship markers {marker_spaceship_distances}')
+    print(f'Distance between base marker(s) and spaceship: {marker_spaceship_distances}')
+
+    if marker_spaceship_distances == -1:
+        if planetDeposit() == -1:
+            drop()
+            reset()
+            return
+
     marker_spaceship_distances_under_300 = list(filter(lambda distance : distance < 300, marker_spaceship_distances))
 
-    print("Distance between base marker(s) and spaceship:", marker_spaceship_distances_under_300)'''
-
-    if seeSpaceship == -1 or spaceship_marker.position.distance > 1000:  # If can't find spaceship or if it is too far from robot
+    print("Distance between base marker(s) and spaceship under 300:", marker_spaceship_distances_under_300)
+    
+    if seeSpaceship == -1 or len(marker_spaceship_distances_under_300) == 0: # If can't find spaceship or it is too far from second base marker
+    #if seeSpaceship == -1 or spaceship_marker.position.distance > 1000:  # If can't find spaceship or if it is too far from robot
         print(f'spaceship distance {spaceship_marker.position.distance}')
-        print('depositing in planet')
-        planetDeposit()
+        if planetDeposit() == -1:
+            drop()
+            reset()
+            return
     else:
         print('depositing in spaceship')
-        spaceshipDeposit(spaceship_marker.id)
+        if spaceshipDeposit(spaceship_marker.id) == -1:
+            drop()
+            reset()
+            return
 
     print('turning')
     fastTurn(False)
