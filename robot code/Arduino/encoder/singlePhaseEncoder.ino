b20e0aba97e771f515581382806f6260700d3f4c
Kingshuk Hossain <kingshukhossain@outlook.com>
2024-03-04 20:52:33 +0000

    Added single phase encoder code

--- robot code/Arduino/encoder/singlePhaseEncoder.ino
+++ robot code/Arduino/encoder/singlePhaseEncoder.ino
@@ -5,7 +5,6 @@
 #define FW_VER 1
 
 #define ENCODER_PIN_A 2
-#define ENCODER_PIN_B 3
 #define WHEEL_DIAMETER 80 // mm
 volatile long encoderCount = 0;
 int lastEncoded = 0;
@@ -17,8 +16,7 @@ void setup()
   pinMode(ENCODER_PIN_B, INPUT);
 
   // Makes change on either pin trigger an interrupt
-  attachInterrupt(digitalPinToInterrupt(ENCODER_PIN_A), encoderISR, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(ENCODER_PIN_B), encoderISR, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_PIN_A), encoderISR, RISING);
 }
 
 int read_pin()
@@ -110,21 +108,7 @@ void loop()
   }
 }
 
-void encoderISR()
+void updateEncoderCount(int phase)
 {
-  int phaseA = digitalRead(ENCODER_PIN_A);
-  int phaseB = digitalRead(ENCODER_PIN_B);
-
-  int encoded = (phaseA << 1) | phaseB; // Combines the two values into one number with a bitwise shift and bitwise OR
-
-  int sum = (lastEncoded << 2) | encoded; // Adds the previous encoder value to the current value to determine direction
-
-  if (sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011)
-    encoderCount++;
-  if (sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000)
-    encoderCount--;
-
-  lastEncoded = encoded; // Store value for next iteration
-
+    encoderCount += 4;
 }
-
