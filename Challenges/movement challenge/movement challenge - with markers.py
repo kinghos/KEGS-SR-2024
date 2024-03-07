8cee4441a7588948539b716a451465d7c40782e7
Kingshuk Hossain <kingshukhossain@outlook.com>
2024-03-07 23:16:05 +0000

    Update to old movement challenge code

--- Challenges/movement challenge/movement challenge - with markers.py
+++ Challenges/movement challenge/movement challenge - with markers.py
@@ -3,67 +3,47 @@ robot = Robot()
 mtrs = robot.motor_board.motors
 ASTEROID_IDS = [i for i in range(150, 200)]
 
+TURNSPEED = 0.25
+DRIVESPEED = 0.4
 def brake():
     '''Sets both motors' power to 0.'''
     mtrs[0].power = 0
     mtrs[1].power = 0
+
+def see():
+    markers = []
+    startTime = robot.time()
+    while not markers:
+        markers = robot.camera.see()
+        robot.sleep(0.05)
+        if time.time() - startTime > 20:
+            print("No markers found")
+            return None
+    return markers
 
 def asteroidScan():
     '''
     Scans the surroundings for asteroids, going clockwise.
     Scans to between -0.02 to 0.02 radians
     '''
-    markers = robot.camera.see()
-    while markers[0].position.horizontal_angle < -0.02 and markers[0].position.horizontal_angle > -0.02:
-        markers = robot.camera.see()
-        mtrs[0].power = 0.1
-        mtrs[1].power = -0.1
+    
+    markers = see()
+    if markers:
+        while markers[0].position.horizontal_angle < -0.02 and markers[0].position.horizontal_angle > -0.02:
+            markers = see()
+            mtrs[0].power = TURNSPEED
+            mtrs[1].power = -TURNSPEED
+    
     brake()
 
 def asteroidApproach():
     '''Approaches the nearest asteroid (the one directly ahead)'''
-    markers = robot.camera.see()
-    while markers[0].position.distance > 30:
-        mtrs[0].power = 0.4
-        mtrs[1].power = 0.4
-
-while True:
-    asteroidScan()
-    asteroidApproach()
-
-    # Turn 90 degrees (needs tweaking)
-    mtrs.power[0] = 0.4
-    mtrs.power[1] = -0.4
-    robot.sleep(1)
-
-    markerCount = 0
-    while True:
-        markers = robot.camera.see()
-
-        if not markers:
-            continue
-        marker = None
-        for i in markers:
-            if i.id == 150 + markerCount:
-                marker = i
-                markerCount += 1
-                markerCount %= 4
-                break
-
-        # Adjust direction if the marker is not straight ahead
-        if marker.position.horizontal_angle < -0.02:
-            mtrs[0].power = 0.1
-            mtrs[1].power = -0.1
-        elif marker.position.horizontal_angle > 0.02:
-            mtrs[0].power = -0.1
-            mtrs[1].power = 0.1
-        else:
-            brake()
-            break
-
-
-    
-    mtrs.power[0] = 0.2
-    mtrs.power[1] = 0.2
-    robot.sleep(1)
+    markers = see()
+    if markers:
+        while markers[0].position.distance > 30:
+            mtrs[0].power = DRIVESPEED
+            mtrs[1].power = DRIVESPEED
     brake()
+
+asteroidScan()
+asteroidApproach()
