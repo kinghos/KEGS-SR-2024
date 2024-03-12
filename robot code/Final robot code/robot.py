c8b64453fd841ea799b4ab68d489b759cd99dd42
Kingshuk Hossain <kingshukhossain@outlook.com>
2024-03-12 20:32:33 +0000

    Updated venv (for convenience)

--- robot code/Final robot code/robot.py
+++ robot code/Final robot code/robot.py
@@ -1,12 +1,14 @@
 # Draft started 24/2/24
 
 from sr.robot3 import *
+from math import pi
 
 robot = Robot()
-motors = robot.motor_board.motors
+driveMotors = robot.motor_board['srXXXX'].motors # Needs serial no.
+mechanismMotors = robot.motor_board['srXXXX'].motors # Needs serial no.
 uno = robot.arduino
 
-CPR = 2 * math.pi / (4 * 11)
+CPR = 2 * pi / (4 * 11)
 WHEEL_DIAMETER = 80
 MARKERSDICT = {
     "boundary": [i for i in range(28)],
@@ -19,8 +21,15 @@ MARKERSDICT = {
 
 def brake():
     '''Stop moving robot.'''
-    motors[0].power = 0
-    motors[1].power = 0
+    driveMotors[0].power = 0
+    driveMotors[1].power = 0
+
+
+def drive(speed):
+    '''Drive robot at a constant speed.'''
+    driveMotors[0].power = speed
+    driveMotors[1].power = speed
+
 
 def findMarker(targetMarker):
     '''Identify a marker based on its ID. Return the marker object if found, else return None.'''
@@ -31,6 +40,7 @@ def findMarker(targetMarker):
     print(f"Couldn't find {targetMarker}")
     return None
 
+
 def findMarkerType(mType):
     '''Identify a marker based on its type. Return the marker object if found, else return None.'''
     markers = robot.camera.see()
@@ -40,13 +50,15 @@ def findMarkerType(mType):
     print(f"Couldn't find {mType}")
     return None
 
+
 def mechanismGrab():
-    '''Pick up and lift an asteroid/egg in front of the robot.'''
-    pass
+    '''Grab an asteroid'''
+    
 
 def mechanismRelease():
     '''Release an asteroid, and lower the arms.'''
     pass
+
 
 def getEncoderDistance():
     encoderCount = int(uno.command("v"))
@@ -54,24 +66,26 @@ def getEncoderDistance():
     print(f"Count: {encoderCount},\t Distance: {distance}mm")
     return distance
 
+
 def approachAsteroid(targetMarker):
     '''Move towards an asteroid, and pick it up.'''
     marker = findMarker(targetMarker)
     while marker.position.horizontal_angle > 0.1:
-        motors[0].power = 0.2
-        motors[1].power = -0.2
+        driveMotors[0].power = 0.2
+        driveMotors[1].power = -0.2
     while marker.position.horizontal_angle < 0.1:
-        motors[0].power = -0.2
-        motors[1].power = 0.2
+        driveMotors[0].power = -0.2
+        driveMotors[1].power = 0.2
     brake()
     robot.sleep(0.1)
     marker = findMarker(targetMarker)
     while marker.position.distance > 30:
-        motors[0].power = 0.4
-        motors[1].power = 0.4
+        drive(0.4)
+
 
 def approachBase():
     pass
+
 
 def main():
     approachAsteroid()
