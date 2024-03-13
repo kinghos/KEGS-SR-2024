14210a9e9f0fcbce040f58f6c395b01c063611ea
Umar <108488645+umar-9@users.noreply.github.com>
2024-03-13 12:29:56 +0000

    Update motor_test.py

--- robot code/Archived/motor_test.py
+++ robot code/Archived/motor_test.py
@@ -1,15 +1,26 @@
 from sr.robot3 import *
 robot = Robot()
 
-mb = robot.motor_board.motors
+drive_board = robot.motor_board["SR0UK1L"].motors
+mech_board = robot.motor_board["SR0KJ15"].motors
 
 def brake():
-    mb[0].power = 0
-    mb[1].power = 0
+    drive_board[0].power = 0
+    drive_board[1].power = 0
 
 print("Moving forwards")
-mb[0].power = 0.4
-mb[1].power = 0.4
+drive_board[0].power = 0.4
+drive_board[1].power = 0.4
+robot.sleep(2)
+print("braking")
+brake()
+robot.sleep(1)
+print("mechanism power")
+mech_board[0].power = 1
+mech_board[1].power = 1
+robot.sleep(2)
+mech_board[0].power = 0
+mech_board[1].power = 0
 
 while True:
     pass
