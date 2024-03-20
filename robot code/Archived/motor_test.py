a5a6e022125047b3863d708e378a3b67e70f3e67
umar-9 <umarsultonov21@gmail.com>
2024-03-20 17:33:33 +0000

    update as per session 20.03.24

--- robot code/Archived/motor_test.py
+++ robot code/Archived/motor_test.py
@@ -3,24 +3,27 @@ robot = Robot()
 
 drive_board = robot.motor_boards["SR0UK1L"].motors
 mech_board = robot.motor_boards["SR0KJ15"].motors
+uno = robot.arduino
 
 def brake():
     drive_board[0].power = 0
     drive_board[1].power = 0
 
-print("Moving forwards")
-drive_board[0].power = 0.4
-drive_board[1].power = 0.4
-robot.sleep(2)
+#print("Moving forwards")
+#drive_board[0].power = 0.4
+#drive_board[1].power = 0.4
+robot.sleep(0.1)
 print("braking")
 brake()
 robot.sleep(1)
 print("mechanism power")
-mech_board[0].power = 1
-mech_board[1].power = 1
-robot.sleep(2)
+mech_board[0].power = -0.6
+robot.sleep(0.6)
 mech_board[0].power = 0
-mech_board[1].power = 0
+robot.sleep(1)
+mech_board[0].power = 0.6
+robot.sleep(0.6)
+mech_board[0].power = 0
 
 while True:
     pass
