7f9094edc81c8cfefc04ebcb82122d27f05c1965
umar-9 <umarsultonov21@gmail.com>
2024-03-20 20:59:32 +0000

    Minor adjustments (added contingencies), refactored

--- robot code/Final robot code/plan B robot.py
+++ robot code/Final robot code/plan B robot.py
@@ -1,6 +1,6 @@
 """
-    Current robot code for plan B (no mechanism)
-
+    Current robot code for plan B+
+    Last tested: 20.03.24 afterschool
 """
 
 from sr.robot3 import *
@@ -16,34 +16,39 @@ CPR = 2 * pi * 1000/ (4*11 * 0.229) # Magic functioning as of 19.03.24
 WHEEL_DIAMETER = 80
 ASTEROID_IDS = [i for i in range(150, 200)]
 BASE_IDS = [i for i in range(8)]
+# BASE_IDS = [i for i in range(robot.zone * 7, (robot.zone + 1) * 7)]
+NUMBER_OF_WALL_MARKERS = 28
+EGG_ID = 110
+PORT_ID = robot.zone + 120
+STARBOARD_ID = robot.zone + 125
 
 TURNSPEED = 0.12
 DRIVESPEED = 0.25
 WAIT = 0.2
-
-TIMEOUT = 40
 
 def brake():
     '''Sets both motors' power to 0.'''
     mtrs[0].power = 0
     mtrs[1].power = 0
 
-'''Speed level of 0 means default speed, and levels above that are added'''
 def turn(clockwise=True, speed_level=0):
+    '''Speed level: defaults to 0, added to the default turnspeed'''
     if clockwise: 
-        mtrs[0].power = -(TURNSPEED+speed_level)%1
-        mtrs[1].power = (2*(TURNSPEED+speed_level))%1
+        mtrs[0].power = -((TURNSPEED + speed_level) % 1)
+        mtrs[1].power = ((2*(TURNSPEED + speed_level)) % 1) # ×2 helps both motors turn so we turn on the spot
     else:
-        mtrs[0].power = (2*(TURNSPEED+speed_level))%1
-        mtrs[1].power = -(TURNSPEED+speed_level)%1
+        mtrs[0].power = ((2*(TURNSPEED + speed_level)) % 1)
+        mtrs[1].power = -((TURNSPEED + speed_level) % 1)
 
 def drive(speed_level=0):
-    mtrs[0].power = DRIVESPEED+speed_level
-    mtrs[1].power = DRIVESPEED+speed_level
+    '''Speed level: defaults to 0, added to the default turnspeed'''
+    mtrs[0].power = (DRIVESPEED+speed_level) % 1
+    mtrs[1].power = (DRIVESPEED+speed_level) % 1
 
-def reverse():
-    mtrs[0].power = -DRIVESPEED
-    mtrs[1].power = -DRIVESPEED
+def reverse(speed_level=0):
+    '''Speed level: defaults to 0, added to the default turnspeed'''
+    mtrs[0].power = -(DRIVESPEED+speed_level)
+    mtrs[1].power = -(DRIVESPEED+speed_level)
 
 
 def getEncoderCount(motor):
@@ -70,15 +75,19 @@ def findTarget(targetid):
     for marker in markers:
         if marker.id == targetid:
             return marker
-    print(f'look couldnt find {targetid}')
+    print(f"Look couldn't find {targetid}")
     return None
 
 
 def closestMarker(clockwise_turn, type):
-    '''Returns None if not found / timeout'''
-    markers = []
+    '''
+    Returns the first marker it sees whilst turning
+    Returns None if not found / timeout
+    '''
     print("closestMarker")
+    markers = []
     startTime = robot.time()
+    TIMEOUT = 12
 
     while len(markers) == 0 and robot.time() - startTime < TIMEOUT:
         markers = [marker for marker in robot.camera.see() if marker.id in type]
@@ -102,25 +111,33 @@ def turnSee(targetid, clockwise_turn, threshold):
     '''
     Scans the surroundings for targetid marker, going clockwise (clockwise_turn = True) or anticlockwise (clockwise_turn = False).
     Scans to between -threshold to threshold radians
+    Returns -1 for if it loses sight of marker during correcting or timeout
     '''
-    print("turnsee")
+    print("turnSee")
     target_marker = findTarget(targetid)
+    startTime = robot.time()
+    TIMEOUT = 12
+
     while target_marker == None:
-        print("Nonefound turnsee")
+        print("None found turnSee")
         turn(clockwise_turn)
         robot.sleep(WAIT)
         brake()
         robot.sleep(WAIT)
         target_marker = findTarget(targetid)
         print(target_marker)
-    print("correctng")
+        if robot.time() - startTime < TIMEOUT:
+            print("turnSee timed out")
+            return -1
+        
+    print("Correcting")
     while target_marker.position.horizontal_angle < -threshold or target_marker.position.horizontal_angle > threshold:
         target_marker = findTarget(targetid)
         if target_marker == None:
             return -1
-        if target_marker.position.horizontal_angle < -3*threshold:
+        if target_marker.position.horizontal_angle < -2.5*threshold:
             turn(False, 0.2)
-        if target_marker.position.horizontal_angle > 3*threshold:
+        if target_marker.position.horizontal_angle > 2.5*threshold:
             turn(True, 0.2)
         elif target_marker.position.horizontal_angle < -threshold:
             turn(False)
@@ -135,51 +152,63 @@ def turnSee(targetid, clockwise_turn, threshold):
 
 
 def markerApproach(targetid, distance, threshold=0.1):
-    print("APPROACH")
-    '''Approaches the nearest asteroid (the one directly ahead)'''
+    '''
+    Approaches the nearest asteroid (the one directly ahead)
+    Returns -1 if loses sight of asteroid
+    '''
+    print("markerApproach")
     target_marker = findTarget(targetid)
     if target_marker == None:
         return -1
     
     prevDistance = calculateDistance(getEncoderCount("left"))
     while target_marker.position.distance > distance:
-        target_marker = findTarget(targetid)
-        if target_marker == None:
-            return -1
-        if target_marker.position.distance <= distance:
-            break
-        print("driving")
-        drive()
+        print("Driving")
+        drive(0.2)
         robot.sleep(WAIT)
         print(f"Motor currents/A: {mtrs[0].current}; {mtrs[1].current}")
         currDistance = calculateDistance(getEncoderCount("left"))
         if currDistance - prevDistance < 20: #if we haven't moved more than 20mm since last repetition of while loop
-            print("We may be stuck...")
-            helpICantSee() #FIXME: change to helpImStuck later
+            print("We are stuck against a wall, but wheels still touch the ground")
+            helpICantSee()
+        #elif motorcurrents dropped v rapidly:
+        #    print("Motors are in the air!")
+        #    helpImStuck()
         brake()
         robot.sleep(WAIT)
         turnSee(target_marker.id, False, threshold)
         prevDistance = currDistance
+        target_marker = findTarget(targetid)
+        if target_marker == None:
+            return -1
     brake()
 
+
 def encoderDrive(distance):
-    print("encoderdrive")
+    """Drive a set distance using encoders"""
+    print("encoderDrive")
+    TIMEOUT = 8
     startTime = robot.time()
     startDistance = calculateDistance(getEncoderCount("left"))
+    prevDistance = startDistance
     print("start: ", startDistance)
     while robot.time() - startTime < TIMEOUT:
         drive()
         encoderCount = getEncoderCount("left")
         encoderDistance = calculateDistance(encoderCount, "left") - startDistance
-        print(f"Encoder distance: {encoderDistance}")
-        print(f"Count: {encoderCount}\t Distance: {encoderDistance}")
+        print(f"Encoder Count: {encoderCount}\t Distance: {encoderDistance}")
         if encoderDistance >= (distance):
             brake()
             return
+        if encoderDistance - prevDistance < 20:
+            print("We are stuck against a wall, but wheels still touch the ground")
+            helpICantSee()
         robot.sleep(0.1)
+        prevDistance = encoderDistance
 
 
 def helpICantSee():
+    """Move us into a better position for seeing"""
     match randint(0,2):
         case 0:
             reverse()
@@ -191,8 +220,25 @@ def helpICantSee():
     brake()
     robot.sleep(WAIT)
 
+
 def helpImStuck():
-    return
+    """Aggressively turn every motor we have randomly"""
+    match randint(0,3):
+        case 0:
+            reverse(1)
+        case 1: 
+            drive(1)
+        case 2:
+            turn(True, 1)
+        case 3:
+            mech_board[0].power = 1
+        case 4:
+            mech_board[0].power = -1
+    robot.sleep(2)
+    brake()
+    mech_board[0].power = 0
+    robot.sleep(WAIT)
+
 
 def release():
     mech_board[0].power = -0.6
@@ -204,18 +250,22 @@ def release():
     robot.sleep(2)
     mech_board[0].power = 0.6
     turn(False, 0.4)
-    robot.sleep(0.8)
+    robot.sleep(0.6)
     mech_board[0].power = 0
     return
 
+
 def eggChecker():
-    if findTarget(110) != None and any([findTarget(base_id) for base_id in BASE_IDS]):
+    if findTarget(EGG_ID) != None and any([findTarget(base_id) for base_id in BASE_IDS]):
         print("I see the egg and a base id at the same time!!!!!")
-        turnSee(110, False, 0.05)
-        markerApproach(110, 700)
+        turnSee(EGG_ID, False, 0.05)
+        markerApproach(EGG_ID, 700)
         encoderDrive(700)
         print("I have got the egg") #FIXME: ADD EGGMOVING CODE TO MOVE TO ANOTHER BASE
         print("eggchecker returning")
+
+
+
 
 def main():
     print("START")
@@ -225,13 +275,14 @@ def main():
         helpICantSee()
         asteroid = closestMarker(True, ASTEROID_IDS)
     robot.sleep(WAIT)
-    ASTEROID_IDS.remove(asteroid.id)
+
     if turnSee(asteroid.id, False, 0.05) == -1:
         main()
         return
     if markerApproach(asteroid.id, 700) == -1:
         main()
         return
+    
     encoderDrive(700)
     robot.sleep(WAIT)
     mtrs[0].power = 0.3
@@ -246,6 +297,7 @@ def main():
         main()
         return
     while markerApproach(base.id, 1500, 0.3) == -1:
+        helpICantSee()
         markerApproach(base.id, 1500, 0.3)
     drive()
     robot.sleep(3)
@@ -253,6 +305,7 @@ def main():
     robot.sleep(WAIT)
 
     release()
+    ASTEROID_IDS.remove(asteroid.id)
 
 while True:
     main()
